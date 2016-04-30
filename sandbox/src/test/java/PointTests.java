38961c15d8379333a961a7175ceca9412386d298
Alik72 <sadvik19@gmail.com>
2016-04-30 19:43:33 +0600

    Урок 2. Задание 3. Создание теста Point

---
+++ sandbox/src/test/java/PointTests.java
@@ -0,0 +1,17 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ru.stqa.pft.sandbox.Point;
+
+/**
+ * Created by Homosapiens on 30.04.2016.
+ */
+public class PointTests {
+
+  @Test
+
+  public void testDistance(){
+    Point p = new Point(1,2,4,6);
+    Assert.assertEquals(p.distance(),5.0);
+
+  }
+}
