6b1fd307c4064c029b02a974a0928c4632908698
Alik72 <sadvik19@gmail.com>
2016-04-30 11:29:32 +0600

    Урок № 1. Создание класса Point.

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,5 +4,17 @@ public class MyFirstProgram {
 
   public static void main(String[] args) {
     System.out.println("Hello, world");
+
+    Point p = new Point(1,2,4,6);
+
+
+    System.out.println("Расстояние между двумя точками" + " p1 " + " и " + " p2 " + " = " + p.distance()  );
+
   }
-}
+
+
+
+
+
+
+  }
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,31 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by Homosapiens on 30.04.2016.
+ */
+public class Point {
+
+  public double x1;
+  public double x2;
+  public double y1;
+  public double y2;
+
+  public static void main(String[] args) {
+
+    Point p = new Point(1,2,4,6);
+
+
+    System.out.println("Расстояние между двумя точками" + " p1 " + " и " + " p2 " + " = " + p.distance()  );
+  }
+
+    public Point (double x1, double y1,double x2, double y2){
+
+    this.x1 = x1;
+    this.y1 = y1;
+    this.x2 = x2;
+    this.y2 = y2;
+  }
+  public double distance() {
+    return Math.sqrt ((Math.pow((this.x2-this.x1), 2)) + (Math.pow((this.y2-this.y1), 2)) );
+  }
+}
